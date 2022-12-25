897d2dd49c69e0d91ad3dde9287d748c2aa660af
Italo Lon kan <sanchotorres777@gmail.com>
2022-12-24 23:34:46 -0500

    Update db name and import routes path

--- index.js
+++ index.js
@@ -1,13 +1,15 @@
 require('dotenv').config();
 
 const express = require('express'); // Server
+const routes = require('./routes/routes');
 const mongoose = require('mongoose');
 const mongoString = process.env.DATABASE_URL;
+
 
 // Database connection - mongodb
 mongoose.set("strictQuery", false);
 
-mongoose.connect(mongoString);
+mongoose.connect(mongoString, { dbName: "prueba2" });
 const database = mongoose.connection;
 
 database.on('error', (error) => {
@@ -23,6 +25,9 @@ database.once('connected', () => {
 const app = express();
 
 app.use(express.json());
+
+// Routes
+app.use('/api', routes)
 
 app.listen(3000, () => {
     console.log(`Server Started at ${3000}`)
