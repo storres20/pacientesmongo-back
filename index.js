3c417e765ba38bdfbbeb7a6124ae2e5fbbb301c9
Italo Lon kan <sanchotorres777@gmail.com>
2022-12-25 13:48:56 -0500

    Add cors and new routes

--- index.js
+++ index.js
@@ -1,7 +1,13 @@
 require('dotenv').config();
 
 const express = require('express'); // Server
-const routes = require('./routes/routes');
+const cors = require("cors");
+
+const products = require('./routes/product.routes');
+const login = require('./routes/login.routes');
+const categories = require('./routes/category.routes');
+const dates = require('./routes/date.routes');
+
 const mongoose = require('mongoose');
 const mongoString = process.env.DATABASE_URL;
 
@@ -9,7 +15,7 @@ const mongoString = process.env.DATABASE_URL;
 // Database connection - mongodb
 mongoose.set("strictQuery", false);
 
-mongoose.connect(mongoString, { dbName: "prueba2" });
+mongoose.connect(mongoString, { dbName: "pacientes" });
 const database = mongoose.connection;
 
 database.on('error', (error) => {
@@ -26,14 +32,25 @@ const app = express();
 
 app.use(express.json());
 
+// Cors
+var corsOptions = {
+    origin: "http://localhost:3000" //frontend
+    //origin: "https://pacientes20.netlify.app" //frontend
+};
+
+app.use(cors(corsOptions));
+
 // simple route
 app.get("/", async (req, res) => {
-  res.json({ message: "Welcome to Back-End application." });
+    res.json({ message: "Welcome to Back-End application." });
 });
 
 // Routes
-app.use('/api', routes)
+app.use('/api/products', products)
+app.use('/api/login', login)
+app.use('/api/categories', categories)
+app.use('/api/dates', dates)
 
-app.listen(3000, () => {
-    console.log(`Server Started at ${3000}`)
+app.listen(3001, () => {
+    console.log(`Server Started at ${3001}`)
 })
